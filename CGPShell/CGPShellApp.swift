3b7c50e86c132180f7c8ab556344b7bd78409771
aanishalien <aanishrizmy28@gmail.com>
2026-07-13 13:09:06 +0530

    Initial Commit

---
+++ CGPShell/CGPShellApp.swift
@@ -0,0 +1,17 @@
+//
+//  CGPShellApp.swift
+//  CGPShell
+//
+//  Created by Nextgen Rentalz on 2026-07-13.
+//
+
+import SwiftUI
+
+@main
+struct CGPShellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
